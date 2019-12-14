bf76525f45efde56c70f5af8fc8b229a819b84c9
Oh <seunghun@bu.edu>
2019-12-14 04:46:57 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './App.css';
+import profile from './resources/profile.jpg';
+import resume from './resources/resume.pdf';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-fixed-picture-div">
+          <img src={profile} className="App-portrait" alt="profile" />
+        </div>
+        <div className="App-content-div">
+          <p>
+            SEUNGHUN OH
+          </p>
+          <a
+            className="App-link"
+            href={resume}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            RESUME
+          </a>
+          <a
+            className="App-link"
+            href="https://github.com/seunghunoh57"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GITHUB
+          </a>
+          <a
+            className="App-link"
+            href="https://linkedin.com/in/seunghunoh"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LINKEDIN
+          </a>
+          
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
