f920a6c38906e08607b93b1dae88443de3203156
Seunghun Oh <seunghun.oh57@gmail.com>
2019-12-14 15:45:25 -0500

    Updates

---
+++ precache-manifest.4464e921f135bf8643fe9a8aff774cf4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2990f3941e01addc5e78a1e04d0a4426",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22fa73218f20810a5d2b",
+    "url": "/static/css/main.dac6f49b.chunk.css"
+  },
+  {
+    "revision": "b83d45674042bb54ffb5",
+    "url": "/static/js/2.82ccb061.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.82ccb061.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22fa73218f20810a5d2b",
+    "url": "/static/js/main.05ee6ee0.chunk.js"
+  },
+  {
+    "revision": "0572bf26b0c5c634eb0e",
+    "url": "/static/js/runtime-main.42e73dbb.js"
+  },
+  {
+    "revision": "7d324c2c865cf0b9b0138ed1c4722539",
+    "url": "/static/media/profile.7d324c2c.jpg"
+  },
+  {
+    "revision": "b4c88e51f924ce5efd05dd29fae9dc05",
+    "url": "/static/media/resume.b4c88e51.pdf"
+  }
+]);
