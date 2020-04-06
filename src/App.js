85e4cc3113b01d04d2c6c9dec8b773349e156fdb
Seunghun Oh <seunghun.oh57@gmail.com>
2020-04-05 21:01:49 -0400

    Initialized footer, prepped for experience entry work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
-import profile from './resources/profile.jpg';
-import resume from './resources/resume.pdf';
+import React from "react";
+import "./App.css";
+import profile from "./resources/profile.jpg";
+import resume from "./resources/resume.pdf";
 
 function App() {
   return (
@@ -11,36 +11,36 @@ function App() {
           <img src={profile} className="App-portrait" alt="profile" />
         </div>
         <div className="App-content-div">
-          <p>
-            SEUNGHUN OH
-          </p>
-          <a
-            className="App-link"
-            href={resume}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            RESUME
-          </a>
-          <a
-            className="App-link"
-            href="https://github.com/seunghunoh57"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GITHUB
-          </a>
-          <a
-            className="App-link"
-            href="https://linkedin.com/in/seunghunoh"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LINKEDIN
-          </a>
-          
+          <p>SEUNGHUN OH</p>
+          <div className="experience-container"></div>
         </div>
       </header>
+      <footer className="App-footer">
+        <a
+          className="App-link"
+          href={resume}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          RESUME
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/seunghunoh57"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GITHUB
+        </a>
+        <a
+          className="App-link"
+          href="https://linkedin.com/in/seunghunoh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LINKEDIN
+        </a>
+      </footer>
     </div>
   );
 }
